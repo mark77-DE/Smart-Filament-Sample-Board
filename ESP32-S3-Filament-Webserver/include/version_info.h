7c28882f89f39790837b7428ce0b5eeae8738249
mark77-DE <forum@mahlen.biz>
2025-12-21 20:53:46 +0100

    Webserver show version

--- ESP32-S3-Filament-Webserver/include/version_info.h
+++ ESP32-S3-Filament-Webserver/include/version_info.h
@@ -1,5 +1,5 @@
 #pragma once
-#define FIRMWARE_VERSION "v0.2.3-dev+716a8e2"
-#define GIT_HASH "716a8e2"
-#define BUILD_DATE "2025-12-21 20:22:45"
+#define FIRMWARE_VERSION "v0.2.3-dev+9df19a0"
+#define GIT_HASH "9df19a0"
+#define BUILD_DATE "2025-12-21 20:51:23"
 #define BUILD_DATE_SHORT "21.12.25"
