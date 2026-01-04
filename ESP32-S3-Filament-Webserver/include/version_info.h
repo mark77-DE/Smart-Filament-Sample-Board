bcd553eb751c2a710b12e8aced523a3acb2dccd0
mark77-DE <forum@mahlen.biz>
2026-01-04 22:11:19 +0100

    prepare release

--- ESP32-S3-Filament-Webserver/include/version_info.h
+++ ESP32-S3-Filament-Webserver/include/version_info.h
@@ -1,5 +1,5 @@
 #pragma once
-#define FIRMWARE_VERSION "v0.2.6-dev+db67b6d"
-#define GIT_HASH "db67b6d"
-#define BUILD_DATE "2026-01-04 22:03:32"
+#define FIRMWARE_VERSION "v0.2.6-dev+52aa9e0"
+#define GIT_HASH "52aa9e0"
+#define BUILD_DATE "2026-01-04 22:09:17"
 #define BUILD_DATE_SHORT "04.01.26"
