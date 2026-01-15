1bd2d94a6848d9334ad378271ac9fa5a9db2858c
mark77-DE <forum@mahlen.biz>
2026-01-15 10:19:15 +0100

    weekend upload

--- ESP32-S3-Filament-Webserver/include/version_info.h
+++ ESP32-S3-Filament-Webserver/include/version_info.h
@@ -1,5 +1,5 @@
 #pragma once
 #define FIRMWARE_VERSION "v0.3.0-dev+b21b80e"
 #define GIT_HASH "b21b80e"
-#define BUILD_DATE "2026-01-15 07:10:16"
+#define BUILD_DATE "2026-01-15 10:17:55"
 #define BUILD_DATE_SHORT "15.01.26"
