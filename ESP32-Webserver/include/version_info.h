b990974459ee8d8130b09a8588154e8b6346b3ba
mark77-DE <forum@mahlen.biz>
2026-08-27 14:33:21 +0200

    webinstaller: add manifests for oled and st7789

--- ESP32-Webserver/include/version_info.h
+++ ESP32-Webserver/include/version_info.h
@@ -1,5 +1,5 @@
 #pragma once
 #define FIRMWARE_VERSION "v0.4.1"
-#define GIT_HASH "9f3d21d"
-#define BUILD_DATE "2026-08-27 12:36:21"
+#define GIT_HASH "2e3ddf5"
+#define BUILD_DATE "2026-08-27 13:34:44"
 #define BUILD_DATE_SHORT "27.08.26"
