2e3ddf5459cd61cfdfd0619ab7068ac225f40029
mark77-DE <forum@mahlen.biz>
2026-08-27 12:45:55 +0200

    Webinstaller first release

--- ESP32-Webserver/include/version_info.h
+++ ESP32-Webserver/include/version_info.h
@@ -1,5 +1,5 @@
 #pragma once
 #define FIRMWARE_VERSION "v0.4.1"
-#define GIT_HASH "65b5e9a"
-#define BUILD_DATE "2026-08-26 17:43:05"
-#define BUILD_DATE_SHORT "26.08.26"
+#define GIT_HASH "9f3d21d"
+#define BUILD_DATE "2026-08-27 12:36:21"
+#define BUILD_DATE_SHORT "27.08.26"
