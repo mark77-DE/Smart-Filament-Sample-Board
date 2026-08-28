a555a45422a7782cdcaad34979de7a254b440864
mark77-DE <forum@mahlen.biz>
2026-08-28 13:17:27 +0200

    FIX pin names for SPI interface in platformio.ini and add SPI defines to the build flags for all environments.

--- ESP32-Webserver/include/version_info.h
+++ ESP32-Webserver/include/version_info.h
@@ -1,5 +1,5 @@
 #pragma once
-#define FIRMWARE_VERSION "v0.4.3"
-#define GIT_HASH "b722c67"
-#define BUILD_DATE "2026-08-28 12:43:39"
+#define FIRMWARE_VERSION "v0.4.4"
+#define GIT_HASH "3cae9bb"
+#define BUILD_DATE "2026-08-28 13:17:02"
 #define BUILD_DATE_SHORT "28.08.26"
