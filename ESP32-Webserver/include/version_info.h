49741e6ac85c3107fd6004378c0a9a039b555347
mark77-DE <forum@mahlen.biz>
2026-08-29 21:10:47 +0200

    Updateintervall changed to 120 minutes. Update check is now done every 2 hours instead of every 3 minutes. This is to reduce the load on the server and to avoid unnecessary update checks.

--- ESP32-Webserver/include/version_info.h
+++ ESP32-Webserver/include/version_info.h
@@ -1,5 +1,5 @@
 #pragma once
-#define FIRMWARE_VERSION "v0.4.6"
-#define GIT_HASH "1af05ec"
-#define BUILD_DATE "2026-08-29 20:50:37"
+#define FIRMWARE_VERSION "v0.4.7"
+#define GIT_HASH "5636856"
+#define BUILD_DATE "2026-08-29 21:10:30"
 #define BUILD_DATE_SHORT "29.08.26"
