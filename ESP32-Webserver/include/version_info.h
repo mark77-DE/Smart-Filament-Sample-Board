58346fd11d0021b41fb1f7c9b6764a1f0389763e
mark77-DE <forum@mahlen.biz>
2026-09-01 16:27:45 +0200

    Default to WS2812B and GRB for LED hardware type and color order in the configuration and settings files. Update the UI to reflect these defaults and ensure that the correct options are displayed based on the selected LED hardware type.

--- ESP32-Webserver/include/version_info.h
+++ ESP32-Webserver/include/version_info.h
@@ -1,5 +1,5 @@
 #pragma once
 #define FIRMWARE_VERSION "v0.4.7"
-#define GIT_HASH "2f78c11"
-#define BUILD_DATE "2026-09-01 16:23:18"
+#define GIT_HASH "121f772"
+#define BUILD_DATE "2026-09-01 16:27:17"
 #define BUILD_DATE_SHORT "01.09.26"
